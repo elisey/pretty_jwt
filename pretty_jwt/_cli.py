8341ed76510e47bf68e681721c304b44828e7b23
Elisei <elisey.rav@gmail.com>
2023-02-09 22:36:03 +0100

    feat: add printing timestamps from jwt token
    Print exp, iat and nbf timestamps in humanreadable format if they exist in jwt token

--- pretty_jwt/_cli.py
+++ pretty_jwt/_cli.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import datetime
 import json
 import sys
+from typing import Any
 
 from ._colorizer import Color, colorize
 from .pretty_jwt import ParceError, parce as parce
@@ -13,6 +15,19 @@ def print_colorized(data: str, color: Color) -> None:
 
 def dict_prettify(data: dict[str, object]) -> str:
     return json.dumps(data, indent=4)
+
+
+def try_print_timestamp(timestamp: Any, claim_name: str) -> None:
+    if not timestamp:
+        return
+
+    try:
+        timestamp_converted = int(timestamp)
+    except ValueError:
+        return
+
+    datetime_str = datetime.datetime.fromtimestamp(timestamp_converted)
+    print_colorized(f"{claim_name}\t: {datetime_str}", Color.MAGENTA)
 
 
 def entrypoint() -> None:
@@ -34,6 +49,17 @@ def entrypoint() -> None:
     print(dict_prettify(jwt.payload))
     print_colorized("Signature:", Color.GREEN)
     print(jwt.signature)
+
+    exp = jwt.payload.get("exp")
+    iat = jwt.payload.get("iat")
+    nbf = jwt.payload.get("nbf")
+
+    if any((exp, iat, nbf)):
+        print()
+
+    try_print_timestamp(exp, "Expiration Time")
+    try_print_timestamp(iat, "Issued At")
+    try_print_timestamp(nbf, "Not Before")
 
 
 if __name__ == "__main__":
